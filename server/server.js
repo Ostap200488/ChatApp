b9665ba2e4b1cb09060df7a7a8b3b156c455fe26
Ostap20048 <135761217+Orest20048@users.noreply.github.com>
2026-01-07 15:52:20 -0330

    Creating of the Server for chat app

--- server/server.js
+++ server/server.js
@@ -2,22 +2,20 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
-
+import { connectDB } from "./lib/db.js";
 // Create Express App and HTTP server
 const app = express();
 const server = http.createServer(app);
-
 // Middleware
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-// ✅ FIXED HEALTH CHECK ROUTE
-app.get("/api/status", (req, res) => {
-  res.send("Server is Online");
-});
+app.get("/api/status", (req, res) => {res.send("Server is Online");});
+
+//Connect to MongoDb
+await connectDB()
 
 const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
-  console.log("Server is Running on PORT " + PORT);
-});
+console.log("Server is Running on PORT " + PORT);});
