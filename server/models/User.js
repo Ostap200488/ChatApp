3e03567955774d2e85c6de3f27ac0792ee6639af
Ostap20048 <135761217+Orest20048@users.noreply.github.com>
2026-01-07 15:56:17 -0330

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     email: {type: String, required: "true", unique: "true"},
-    fullName: {type: String, required: "true"},
-    password: {type: String, required: "true"},
-})
+    fullName: {type: String, required: "true",},
+    password: {type: String, required: "true", minlengh: 6 },
+    prifilePic: {type: String, default: "" },
+    bio: {type: String},
+},{timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User
