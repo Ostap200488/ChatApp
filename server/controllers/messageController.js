886ebe2e5a2df89b3213e8ada9db2556b3bb824f
Ostap20048 <135761217+Orest20048@users.noreply.github.com>
2026-01-08 10:52:15 -0330

    Updating of the message controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,5 +1,6 @@
 import Message from "../models/Message";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
 
 //Get all users except the logged user 
 export const getUsersForSidebar = async(req, res)=> {
@@ -31,12 +32,51 @@ export const getMessages = async (req , res) => {
 
         const messages = await Message.find({
             $or : [
-                {senderId:myId, receiverId, selectedUserId}
+                {senderId:myId, receiverId, selectedUserId},
+                {senderId: selectedUserId, receiverId:myId}
             ]
         })
+        await Message.updateMany({senderId:selectedUserId,receiverId: myId}, {seen: true});
+
+        res.json({success: true, messages})
 
 
+    } catch (error) {
+        console,log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+// Api to mark message as seen using message id 
+export const markMessageAsSeen = async(req , res)=> {
+    try { 
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id , {seen: true})
+        res.json({success: true})
+    } catch (error) {
+        console,log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+// Send message to selected user 
+export const sendMessage = async (req , res)=> {
+    try {
+        const {text , image } = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
 
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        res.json({success: true, newMessage});
     } catch (error) {
         console,log(error.message);
         res.json({success: false, message: error.message})
