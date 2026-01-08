6fda752a8f43a27aebbf4388b03d4e894dca0d1d
Ostap20048 <135761217+Orest20048@users.noreply.github.com>
2026-01-07 23:19:21 -0330

    Update messageController.js

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,8 +1,44 @@
+import Message from "../models/Message";
+import User from "../models/User.js";
+
 //Get all users except the logged user 
 export const getUsersForSidebar = async(req, res)=> {
     try {
-        const userId = req.user
+        const userId = req.user._id;
+        const filteredUsers = await User.find({_id:{$ne:userId}}).select("-password");
+
+        //Count the number of non seen messages 
+        const unseenMessages = {}
+        const promises = filteredUsers.map(async ()=>{
+            const messages = await Message.find({senderId: user._id,  
+                receiverId:userId, seem: false})
+                if(messages.length > 0){
+                    unseenMessages[user._id] = messages.length;
+                }
+        })
+        await Promise.all(promises)
+        res.json({success: true, users: filteredUsers , unseenMessages })
     } catch (error) {
-        
+        console.log(error.messages);
+        res.json({success: false, message: error.message })
+    }
+}
+//Get all messages for selected user 
+export const getMessages = async (req , res) => {
+    try {
+        const {id: selectedUserId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or : [
+                {senderId:myId, receiverId, selectedUserId}
+            ]
+        })
+
+
+
+    } catch (error) {
+        console,log(error.message);
+        res.json({success: false, message: error.message})
     }
 }
