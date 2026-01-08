595ad68cff0482c250d382f0407e370982797cab
Ostap20048 <135761217+Orest20048@users.noreply.github.com>
2026-01-08 11:06:53 -0330

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,33 @@ import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 import { Server } from "socket.io";
+
 // Create Express App and HTTP server
 const app = express();
 const server = http.createServer(app);
+
+// Iniate socket_is server 
+export const io = new Server(server, {
+    cors: {origin:"*"}
+})
+
+//Store onile user 
+export const userSocketMap = {}; // {userId:socketId}
+
+//Socket.io connection handler 
+io.on("connection", (socket)=> {
+    const  userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    //Emit online users to all connected client 
+    io.emit("getOnlineUser", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=> {
+        console.log("User Disconnected", userId)
+    })
+})
 // Middleware
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
