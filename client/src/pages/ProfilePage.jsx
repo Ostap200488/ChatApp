cc4ea9821b75fc9ca6304a8417541742dbee1fc2
Ostap20048 <135761217+Orest20048@users.noreply.github.com>
2026-01-09 19:07:33 -0330

    Adding user profile  edit function

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {useNavigate} from 'react-router-dom'
 import assets from "../assets/assets";
+import { AuthContext } from "../../context/AuthContext";
 
 const ProfilePage = () => {
+
+
+    const {authUser,updateProfile} = useContext(AuthContext)
 
     const [selectedImg, setSelectedImg] = useState(null)
     const navigate = useNavigate();
@@ -11,7 +15,20 @@ const ProfilePage = () => {
 
     const handleSubmit = async (e)=> {
         e.preventDefault();
-        navigate("/")
+        if(!selectedImg){
+            await updateProfile({fullName: name, bio}); 
+            navigate("/")
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.readAsDataURL(selectedImg);
+        reader.onload = async ()=> {
+            const base46Image = reader.result;
+            await updateProfile({profilePic: base46Image, fullName: name, bio})
+            navigate("/");
+        }
+      
     }
 
     return (
